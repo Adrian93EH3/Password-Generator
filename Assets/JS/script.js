6078ff66ff5b95d69c85e35827fc77150581a01d
Adrian Diaz <adrian93eh3@gmail.com>
2020-08-01 14:18:22 -0400

    forgot what I was commiting

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,8 +2,11 @@ var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}'];
 
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var passwordLength = parseInt(prompt('How long do you want your password to be?'))
 
@@ -16,8 +19,11 @@ if (confirm('Would you like symbols?')) {
 if (confirm('Would you like numbers?')) {
     passwordGenerated.push(numbers)
 }
-if (confirm('Would you like letters?')) {
-    passwordGenerated.push(letters)
+if (confirm('Would you like lowercase letters?')) {
+    passwordGenerated.push(lowercaseLetters)
+}
+if (confirm('Would you like uppercase letters?')) {
+    passwordGenerated.push(uppercaseLetters)
 }
 console.log(passwordLength, passwordGenerated);
 
