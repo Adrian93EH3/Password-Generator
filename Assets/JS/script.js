d9c12c061fbcc93a12f0875e0c92179b673854cd
Adrian Diaz <adrian93eh3@gmail.com>
2020-08-01 14:32:42 -0400

    restarting page with new JS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,39 +1,9 @@
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}'];
+// grabs the elements by their ID in the HTML and I gave them new parameters to reference them by in future code
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-
-var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-var passwordLength = parseInt(prompt('How long do you want your password to be?'))
-
-var passwordGenerated = []
-var randomArray = []
-var randomString = ''
-
-if (confirm('Would you like symbols?')) {
-    passwordGenerated.push(symbols)
-}
-if (confirm('Would you like numbers?')) {
-    passwordGenerated.push(numbers)
-}
-if (confirm('Would you like lowercase letters?')) {
-    passwordGenerated.push(lowercaseLetters)
-}
-if (confirm('Would you like uppercase letters?')) {
-    passwordGenerated.push(uppercaseLetters)
-}
-console.log(passwordLength, passwordGenerated);
-
-for (var i = 0; i < passwordLength; i++) {
-    var randomPassword = Math.floor(Math.random() * passwordGenerated.length);
-    var randomPasswordPicker = Math.floor(Math.random() * passwordGenerated[randomPassword].length);
-    var random = passwordGenerated[randomPassword][randomPasswordPicker]
-    randomArray.push(random)
-    randomString += random
-    console.log(randomPassword, randomPasswordPicker, random)
-}
-console.log(randomArray)
