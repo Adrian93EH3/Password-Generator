90d292a91509d1c7469d3f566e4868a97810a791
Adrian Diaz <adrian93eh3@gmail.com>
2020-08-01 14:48:39 -0400

    changed names of some value to type it out faster as well as add an EventListener that checks what has been checked in the HTML checkboxes to add into the generator

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,29 +9,46 @@ const generateEl = document.getElementById('generate');
 
 // renaming values for functions down below on lines 18~36
 const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
+    lower: randomLower,
+    upper: randomUpper,
+    number: randomNumber,
+    symbol: randomSymbol
 }
 
 // chooses lowercase characters from ASCII list 97~122 aka a~z
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function randomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 // chooses uppercase characters from ASCII list 65~90 aka A~Z
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function randomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 // chooses numbers from ASCII list 48~57 aka 0-9
-function getRandomNumber() {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+function randomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 // chooses random symbols from list of values below
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
+function randomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+// checks what has been selected in the HTML to be generated
+generate.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// generates what has been selected to generate
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
