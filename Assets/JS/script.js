d509616bb4c240f8863df01f130a900d2b74b7f0
Adrian Diaz <adrian93eh3@gmail.com>
2020-08-01 10:29:28 -0400

    adding a password length array

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,3 +4,6 @@ var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var passwordLength = parseInt(prompt('How long do you want your password to be?'))
+
